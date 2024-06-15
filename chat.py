e7364cc645003473b336984e17d9a98bd7036e45
weihanlairrr <141799681+weihanlairrr@users.noreply.github.com>
2024-06-15 22:09:54 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import base64
-from openai import OpenAI
 from streamlit_option_menu import option_menu
-import ollama  # 新增的匯入
+import replicate
+import os
 
 # Custom CSS for slider color和button styles
 st.markdown("""
@@ -15,8 +15,8 @@ st.markdown("""
         border-radius: 5px;
         font-size: 18px;
         cursor: pointer;
-        margin: 1px 0; /* 僅設定上下邊距，消除左右邊距 */
-        width: 100%; /* 讓按鈕自動佔滿整欄 */
+        margin: 1px 0;
+        width: 100%;
     }
     .stButton > button:hover {
         background-color: #3399FF;
@@ -32,13 +32,11 @@ def get_image_as_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
-# 根據模型選擇載入對應的 avatar 圖片
 assistant_avatar_gpt = get_image_as_base64("Images/ChatGPT Logo.png")
 assistant_avatar_llama = get_image_as_base64("Images/Meta Logo.png")
-assistant_avatar = assistant_avatar_gpt  # 默認為 GPT 模型的 avatar
+assistant_avatar = assistant_avatar_gpt
 user_avatar_default = get_image_as_base64("Images/Asian Man.png")
 
-# 加載所有頭像圖片
 avatars = {
     "Asian Man": get_image_as_base64("Images/Asian Man.png"),
     "Robot": get_image_as_base64("Images/Robot.png"),
@@ -51,15 +49,21 @@ avatars = {
     "Asian Girl": get_image_as_base64("Images/Asian Girl.png"),
 }
 
-# 預設使用者頭像
 if 'user_avatar' not in st.session_state:
     st.session_state['user_avatar'] = user_avatar_default
 
-# 初始化 session state 變數
 if 'chatbot_api_key' not in st.session_state:
     st.session_state['chatbot_api_key'] = ''
+if 'replicate_api_key' not in st.session_state:
+    st.session_state['replicate_api_key'] = ''
 if 'open_ai_model' not in st.session_state:
     st.session_state['open_ai_model'] = 'gpt-3.5-turbo'
+if 'llama_temperature' not in st.session_state:
+    st.session_state['llama_temperature'] = 0.5
+if 'llama_top_p' not in st.session_state:
+    st.session_state['llama_top_p'] = 1.0
+if 'llama_presence_penalty' not in st.session_state:
+    st.session_state['llama_presence_penalty'] = 0.0
 if 'language' not in st.session_state:
     st.session_state['language'] = ''
 if 'temperature' not in st.session_state:
@@ -76,7 +80,7 @@ if 'tabs' not in st.session_state:
     st.session_state['tabs'] = ["對話 1"]
     st.session_state['current_tab'] = 1
     st.session_state[f"messages_ChatGPT_1"] = [{"role": "assistant", "content": "請輸入您的 OpenAI API Key" if not st.session_state['chatbot_api_key'] else "請問需要什麼協助？"}]
-    st.session_state[f"messages_Llama3_1"] = [{"role": "assistant", "content": "請問需要什麼協助？"}]
+    st.session_state[f"messages_Llama3_1"] = [{"role": "assistant", "content": "請輸入您的 Replicate API Key" if not st.session_state['replicate_api_key'] else "請問需要什麼協助？"}]
     st.session_state[f"tab_name_1"] = "對話 1"
 if 'chat_started' not in st.session_state:
     st.session_state['chat_started'] = False
@@ -85,7 +89,6 @@ if 'api_key_removed' not in st.session_state:
 if 'model_type' not in st.session_state:
     st.session_state['model_type'] = 'ChatGPT'
 
-# 定義一個函數來顯示頭像選項
 def display_avatars():
     cols = st.columns(3)
     for i, (name, image) in enumerate(avatars.items()):
@@ -116,12 +119,14 @@ def get_openai_response(client, model, messages, temperature, top_p, presence_pe
 
 def generate_ollama_response(prompt):
     try:
-        response = ollama.chat(model='llama3', stream=True, messages=[{"role": "user", "content": prompt}])
-        full_message = ""
-        for partial_resp in response:
-            token = partial_resp["message"]["content"]
-            full_message += token
-        return full_message
+        response = replicate.run(
+            "meta/meta-llama-3-8b-instruct",
+            input={"prompt": prompt, "temperature": st.session_state['llama_temperature'], "top_p": st.session_state['llama_top_p'], "presence_penalty": st.session_state['llama_presence_penalty']}
+        )
+        # 將 response 轉換為字符串並移除多餘的空格
+        response_str = ''.join(response) if isinstance(response, list) else str(response)
+        response_str = ' '.join(response_str.split())
+        return response_str
     except Exception as e:
         return f"Error: {str(e)}"
 
@@ -138,7 +143,10 @@ def confirm_reset_chat():
 
 def reset_chat():
     key = f"messages_{st.session_state['model_type']}_{st.session_state['current_tab']}"
-    st.session_state[key] = [{"role": "assistant", "content": "請輸入您的 OpenAI API Key" if st.session_state['model_type'] == 'ChatGPT' and not st.session_state['chatbot_api_key'] else "請問需要什麼協助？"}]
+    if st.session_state['model_type'] == 'ChatGPT':
+        st.session_state[key] = [{"role": "assistant", "content": "請輸入您的 OpenAI API Key" if not st.session_state['chatbot_api_key'] else "請問需要什麼協助？"}]
+    else:
+        st.session_state[key] = [{"role": "assistant", "content": "請輸入您的 Replicate API Key" if not st.session_state['replicate_api_key'] else "請問需要什麼協助？"}]
     st.session_state['reset_confirmation'] = False
     st.session_state['chat_started'] = False
     st.session_state['api_key_removed'] = False
@@ -201,63 +209,56 @@ def message_func(text, is_user=False, is_df=False):
         )
 
 with st.sidebar:
-    model_toggle = st.radio(" ", ["ChatGPT", "Llama3"], key="model_type", horizontal=True)
+    model_toggle = st.radio("", ["ChatGPT", "Llama3"], key="model_type", horizontal=True)
     
     selected = option_menu("主頁", ["對話", '頭像', '模型設定'], 
         icons=['chat-left-dots', 'person-circle', 'gear'], menu_icon="cast", default_index=0,
         styles={
         "container": {"padding": "0.5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "22x"}, 
+        "icon": {"color": "orange", "font-size": "22px"}, 
         "nav-link": {"font-size": "19px", "text-align": "left", "margin":"5px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#006AFF"}})
     
     # 根據模型選擇設置 avatar
     if model_toggle == "Llama3":
         assistant_avatar = assistant_avatar_llama
+        if selected == "對話":
+            replicate_api_key_input = st.text_input("請輸入 Replicate API Key", value=st.session_state.get('replicate_api_key', ''), type="password")
+            if replicate_api_key_input != st.session_state['replicate_api_key']:
+                st.session_state['replicate_api_key'] = replicate_api_key_input
+                os.environ["REPLICATE_API_TOKEN"] = replicate_api_key_input
+                st.session_state[f"messages_Llama3_{st.session_state['current_tab']}"][0]['content'] = "請問需要什麼協助？" if replicate_api_key_input else "請輸入您的 Replicate API Key"
+                st.experimental_rerun()
     else:
         assistant_avatar = assistant_avatar_gpt
-    
-    if selected == "對話":
-        if model_toggle == "ChatGPT":
+        if selected == "對話":
             api_key_input = st.text_input("請輸入 OpenAI API Key", value=st.session_state.get('chatbot_api_key', ''), type="password")
             if api_key_input != st.session_state['chatbot_api_key']:
                 st.session_state['chatbot_api_key'] = api_key_input
-                if api_key_input == '':
-                    if not st.session_state['chat_started']:
-                        reset_chat()
-                    else:
-                        st.session_state['api_key_removed'] = True
-                else:
-                    if not st.session_state['chat_started']:
-                        st.session_state[f"messages_{st.session_state['model_type']}_{st.session_state['current_tab']}"][0]['content'] = "請問需要什麼協助？"
-                    st.session_state['api_key_removed'] = False
+                st.session_state[f"messages_ChatGPT_{st.session_state['current_tab']}"][0]['content'] = "請問需要什麼協助？" if api_key_input else "請輸入您的 OpenAI API Key"
                 st.experimental_rerun()
-
-            model_input = st.selectbox("選擇 GPT 模型", ("gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o"), index=("gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o").index(st.session_state.get('open_ai_model', 'gpt-3.5-turbo')))
-            if model_input != st.session_state['open_ai_model']:
-                st.session_state['open_ai_model'] = model_input
-                st.experimental_rerun()
-
+    
+    if selected == "對話":
         st.divider()
         st.button("重置對話", on_click=lambda: st.session_state.update({'reset_confirmation': True}), use_container_width=True)
         
         if st.session_state['reset_confirmation']:
             confirm_reset_chat()
-
+    
     elif selected == "頭像":
         st.write("選擇您的頭像")
         display_avatars()
         st.write("\n")
         st.markdown(f"""
-    <div style='text-align: center;'>
-        <img src="data:image/png;base64,{st.session_state['user_avatar']}" style='width: 200px;'/>
-        <p>\n</p>
-    </div>
-""", unsafe_allow_html=True)
-            
+            <div style='text-align: center;'>
+                <img src="data:image/png;base64,{st.session_state['user_avatar']}" style='width: 200px;'/>
+                <p>\n</p>
+            </div>
+        """, unsafe_allow_html=True)
+        
         st.markdown("<p style='text-align: center;'>目前的頭像</p>", unsafe_allow_html=True)
         st.markdown("</div>", unsafe_allow_html=True)
-            
+        
     elif selected == "模型設定":
         st.session_state['language'] = st.text_input("指定使用的語言", value=st.session_state.get('language'), placeholder="預設為繁體中文")
         if model_toggle == "ChatGPT":
@@ -265,24 +266,32 @@ with st.sidebar:
             st.session_state['top_p'] = st.select_slider("選擇 Top P", options=[i/10.0 for i in range(11)], value=st.session_state.get('top_p', 1.0), help="基於核心機率的採樣，模型會考慮概率最高的top_p個標記的預測結果。當該參數為0.1時，代表只有包括前10%概率質量的標記將被考慮。一般建議只更改這個參數或 Temperature 中的一個，而不要同時更改。")
             st.session_state['presence_penalty'] = st.select_slider("選擇 Presence Penalty", options=[i/10.0 for i in range(-20, 21)], value=st.session_state.get('presence_penalty', 0.0), help="正值會根據新標記是否出現在當前生成的文本中對其進行懲罰，從而增加模型談論新話題的可能性。")
             st.session_state['frequency_penalty'] = st.select_slider("選擇 Frequency Penalty", options=[i/10.0 for i in range(-20, 21)], value=st.session_state.get('frequency_penalty', 0.0), help="正值會根據新標記是否出現在當前生成的文本中對其進行懲罰，從而增加模型談論新話題的可能性。")
+        else:
+            st.session_state['llama_temperature'] = st.select_slider("選擇 Temperature", options=[i/10.0 for i in range(11)], value=st.session_state.get('llama_temperature', 0.5), help="較高的值會使輸出更隨機，而較低的值則會使其更加集中和確定性。")
+            st.session_state['llama_top_p'] = st.select_slider("選擇 Top P", options=[i/10.0 for i in range(11)], value=st.session_state.get('llama_top_p', 1.0), help="基於核心機率的採樣，模型會考慮概率最高的top_p個標記的預測結果。當該參數為0.1時，代表只有包括前10%概率質量的標記將被考慮。")
+            st.session_state['llama_presence_penalty'] = st.select_slider("選擇 Presence Penalty", options=[i/10.0 for i in range(-20, 21)], value=st.session_state.get('llama_presence_penalty', 0.0), help="正值會根據新標記是否出現在當前生成的文本中對其進行懲罰，從而增加模型談論新話題的可能性。")
 
 # 根據模型選擇設置當前對話
 current_tab_key = f"messages_{st.session_state['model_type']}_{st.session_state['current_tab']}"
 if current_tab_key not in st.session_state:
-    st.session_state[current_tab_key] = [{"role": "assistant", "content": "請輸入您的 OpenAI API Key" if st.session_state['model_type'] == 'ChatGPT' and not st.session_state['chatbot_api_key'] else "請問需要什麼協助？"}]
+    st.session_state[current_tab_key] = [{"role": "assistant", "content": "請輸入您的 OpenAI API Key" if st.session_state['model_type'] == "ChatGPT" and not st.session_state['chatbot_api_key'] else "請輸入您的 Replicate API Key" if st.session_state['model_type'] == "Llama3" and not st.session_state['replicate_api_key'] else "請問需要什麼協助？"}]
 
 # 顯示對話
 for msg in st.session_state[current_tab_key]:
     message_func(msg["content"], is_user=(msg["role"] == "user"))
 
 # 處理新訊息
-if st.session_state['chatbot_api_key'] or st.session_state['model_type'] == "Llama3":
+if st.session_state['chatbot_api_key'] or st.session_state['replicate_api_key']:
     prompt = st.chat_input()
     if prompt:
         st.session_state['chat_started'] = True
         if st.session_state['model_type'] == "ChatGPT" and not st.session_state['chatbot_api_key']:
             if st.session_state['api_key_removed'] or not st.session_state['chat_started']:
                 st.session_state[current_tab_key].append({"role": "assistant", "content": "請輸入您的 OpenAI API Key"})
+            st.experimental_rerun()
+        elif st.session_state['model_type'] == "Llama3" and not st.session_state['replicate_api_key']:
+            if st.session_state['api_key_removed'] or not st.session_state['chat_started']:
+                st.session_state[current_tab_key].append({"role": "assistant", "content": "請輸入您的 Replicate API Key"})
             st.experimental_rerun()
         else:
             st.session_state['api_key_removed'] = False
